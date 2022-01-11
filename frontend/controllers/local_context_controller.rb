164a796a33df176e191ae46eff7045eac855eea2
Joshua Shaw <joshua.d.shaw@dartmouth.edu>
2022-01-10 19:48:26 -0500

    add some error checking and logging

--- frontend/controllers/local_context_controller.rb
+++ frontend/controllers/local_context_controller.rb
@@ -7,16 +7,20 @@ class LocalContextController < ApplicationController
 set_access_control "view_repository" => [:fetch_lc_data]
 
   def fetch_lc_data
+    logger = Logger.new($stderr)
     api_base_url = AppConfig[:local_context_api]
     project_id = params[:project_id]
 
     uri = URI(File.join(api_base_url, 'projects', project_id, '?format=json'))
+    logger.debug("LC URI: #{uri.inspect}")
     res = Net::HTTP.get_response(uri)
-
+    logger.debug("LC_RESPONSE: #{res.inspect}")
     if res.is_a?(Net::HTTPSuccess)
       render :json => res.body
     else
       render :json => {"error":"Something went wrong. Unable to fetch and/or parse data for this project id."}
     end
+  rescue Timeout::Error => e
+    render :json => {"error":"Timeout attempting to fetch data from Local Contexts API. Unable to fetch and/or parse data for this project id. Error message: #{e}"}
   end
 end
