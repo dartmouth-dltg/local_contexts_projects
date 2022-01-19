2e8b31beed48cd4222e36055b47f63878c7f9abd
Joshua Shaw <joshua.d.shaw@dartmouth.edu>
2022-01-19 10:48:57 -0500

    inheritance added

--- indexer/local_contexts_project_indexer.rb
+++ indexer/local_contexts_project_indexer.rb
@@ -18,6 +18,18 @@ class IndexerCommon
             doc['local_contexts_project_uris_u_sstr'] << lcp['ref']
           end
         end
+
+        if doc['primary_type'] == 'archival_object'
+          doc['inherited_local_contexts_projects_u_sstr'] = []
+          # only check if the object is not already tagged
+          if doc['local_contexts_project_uris_u_sstr'].empty?
+            if record['record']['parent']
+              get_parent_local_context_uris(record['record']['parent']['ref'], doc)
+            elsif record['record']['resource']
+              get_parent_local_context_uris(record['record']['resource']['ref'], doc)
+            end
+          end
+        end
       }
     end
   end
@@ -34,5 +46,22 @@ class IndexerCommon
     }
   end
 
+  def self.get_parent_local_context_uris(uri, doc)
+    inherited_lcps = []
+    parent = JSONModel::HTTP.get_json(uri)
+    parent['local_contexts_projects'].each do |lcp|
+      inherited_lcps << JSONModel::HTTP.get_json(lcp['ref'])
+    end
+    if inherited_lcps.length > 0
+      doc['inherited_local_contexts_projects_u_sstr'] << {'local_contexts_projects' => inherited_lcps, 'level' => parent['level'], 'parent_uri' => parent['uri']}.to_json
+    end
+    if doc['inherited_local_contexts_projects_u_sstr'].empty?
+      if parent['parent']
+        get_parent_local_context_uris(parent['parent']['ref'], doc)
+      elsif parent['resource']
+        get_parent_local_context_uris(parent['resource']['ref'], doc)
+      end
+    end
+  end
 
 end
